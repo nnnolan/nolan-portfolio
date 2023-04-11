01815f46605cd79297f0dcd2a8770c79fc4d6836
nnnolan <pestanonolan@gmail.com>
2023-04-11 09:03:53 -0500

    part two of gatsby!

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby. Hello, World!</p>
+    </Layout>
   )
-}
+  }
 
 export const Head = () => <title>About Me</title>
 
