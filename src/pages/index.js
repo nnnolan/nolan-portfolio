02eadd5feaa7a8fb14bd6a66047e577e04362179
nnnolan <pestanonolan@gmail.com>
2023-08-22 10:21:51 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
     <Layout pageTitle="🧙‍♂️🃏">
       <h1>nolan pestano</h1>
       <p>Hello World! I am Nolan Pestano, and this is my page.</p>
-      <p> I just recently graduated from Smithville High School, but I plan on attending Mizzou this fall. Most recently, I was with UGA Finance's backend process team in KC, MO.</p>
+      <p> I am a current student of Computer Science @ the Univeristy of Missouri - Columbia. Most recently, I was with UGA Finance's backend process team in KC, MO.</p>
       <p> I enjoy Oxford Commas, Chess, Python, and Good Coffee, among other things. Above all, I love life and love being a nerd.</p>
       <StaticImage
         alt= "Summer Night by Winslow Homer, a personal favorite"
